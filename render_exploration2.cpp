769dfaf176241d25e73485ff93258d68ce95752d
GreenMoonMoon <josue.boisvert@gmail.com>
2022-02-04 21:39:52 -0500

    encapsulate gl context and window creation from

--- render_exploration2.cpp
+++ render_exploration2.cpp
@@ -1,127 +1,36 @@
 //
 // Created by MoonMoon on 2022-01-17.
 //
-#include <iostream>
-
-#include "glad/gl.h"
-#include "GLFW/glfw3.h"
-
 #include "src/render/render.h"
 #include "src/geometry.h"
 
-using namespace Expl;
-
-void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-
-void ProcessInput(GLFWwindow *window);
-
-GLenum glCheckError_(const char *file, int line);
-
-#define glCheckError() glCheckError_(__FILE__, __LINE__)
+using Expl::Shader;
+using Expl::MeshResource;
+using Expl::Mesh;
+using Expl::Renderer;
 
 int main() {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-//    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // MacOS
-    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+    Renderer renderer{};
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Render Exploration 1", nullptr, nullptr);
-    if (window == nullptr) {
-        std::cout << "Could not create a window!" << '\n';
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-
-    int version = gladLoadGL(glfwGetProcAddress);
-    if (version == 0) {
-        std::cout << "Could not initialize glad!" << '\n';
-        glfwTerminate();
-        return -1;
-    }
-
-    glViewport(0, 0, 800, 600);
-
-
-    //Register resize function
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    //Shader
-    Shader shader = Shader(
+    auto shader = Shader(
             "C:/Users/josue/CLionProjects/PhysicExploration/resources/shaders/basic.vert",
             "C:/Users/josue/CLionProjects/PhysicExploration/resources/shaders/basic.frag"
     );
 
-    MeshResource res = MeshResource::Quad();
-    Mesh mesh = Renderer::LoadMeshResource(res);
+    auto res = MeshResource::Quad();
+    auto mesh = Renderer::LoadMeshResource(res);
 
 
-    while (!glfwWindowShouldClose(window)) {
-        //input
-        ProcessInput(window);
+    while (!renderer.ShouldClose()) {
+        renderer.ProcessInput();
 
-        //render
-        glClearColor(0.55f, 0.55f, 0.55f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        renderer.BeginDraw();
 
         Renderer::DrawMesh(mesh, shader);
-        glCheckError();
+        Expl::glCheckError();
 
-        glfwPollEvents();
-        glfwSwapBuffers(window);
+        renderer.EndDraw();
     }
-
-    //Cleanup
-
-    glfwDestroyWindow(window);
-    glfwTerminate();
 
     return 0;
-}
-
-void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
-    glViewport(0, 0, width, height);
-}
-
-/// Process input for a given window
-/// \param window
-void ProcessInput(GLFWwindow *window) {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
-}
-
-GLenum glCheckError_(const char *file, int line) {
-    GLenum errorCode;
-    while (errorCode = glGetError(), errorCode != GL_NO_ERROR) {
-        std::string error;
-        switch (errorCode) {
-            case GL_INVALID_ENUM:
-                error = "INVALID_ENUM";
-                break;
-            case GL_INVALID_VALUE:
-                error = "INVALID_VALUE";
-                break;
-            case GL_INVALID_OPERATION:
-                error = "INVALID_OPERATION";
-                break;
-            case GL_STACK_OVERFLOW:
-                error = "STACK_OVERFLOW";
-                break;
-            case GL_STACK_UNDERFLOW:
-                error = "STACK_UNDERFLOW";
-                break;
-            case GL_OUT_OF_MEMORY:
-                error = "OUT_OF_MEMORY";
-                break;
-            case GL_INVALID_FRAMEBUFFER_OPERATION:
-                error = "INVALID_FRAMEBUFFER_OPERATION";
-                break;
-            default:
-                break;
-        }
-        std::cout << error << " | " << file << " (" << line << ")" << '\n';
-    }
-    return errorCode;
 }
--- src/render/renderer.h
+++ src/render/renderer.h
@@ -1,16 +1,25 @@
 //
 // Created by MoonMoon on 2022-01-29.
 //
+#include <iostream>
 
 #include "glad/gl.h"
+#include "GLFW/glfw3.h"
 #include "../geometry.h"
 
 #ifndef EXPLORATION_RENDERER_H
 #define EXPLORATION_RENDERER_H
 
 namespace Expl {
+
+    bool GLFW_INITIALIZED = false;
+
+    void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+        glViewport(0, 0, width, height);
+    }
+
     struct Mesh {
-        Resource* resource = nullptr;
+        Resource *resource = nullptr;
         unsigned int VBO = 0;
         unsigned int VAO = 0;
         unsigned int EBO = 0;
@@ -20,7 +29,64 @@ namespace Expl {
     };
 
     class Renderer {
+    private:
+        GLFWwindow *window;
+
     public:
+        Renderer() {
+            if (!GLFW_INITIALIZED) GLFW_INITIALIZED = glfwInit();
+
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+            glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+            glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // MacOS
+            glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+            window = glfwCreateWindow(800, 600, "Render Exploration 1", nullptr, nullptr);
+            if (window == nullptr) {
+                std::cout << "Could not create a window!" << '\n';
+                glfwTerminate();
+                return;
+            }
+            glfwMakeContextCurrent(window);
+
+            int version = gladLoadGL(glfwGetProcAddress);
+            if (version == 0) {
+                std::cout << "Could not initialize glad!" << '\n';
+                glfwTerminate();
+                return;
+            }
+
+            glViewport(0, 0, 800, 600);
+
+            //Register resize function
+            glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+        }
+
+        ~Renderer() {
+            glfwDestroyWindow(window);
+            glfwTerminate();
+        }
+
+        bool ShouldClose() {
+            return glfwWindowShouldClose(window);
+        }
+
+        void ProcessInput() {
+            if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+                glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }
+
+        void BeginDraw() {
+            glClearColor(0.55f, 0.55f, 0.55f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        }
+
+        void EndDraw() {
+            glfwPollEvents();
+            glfwSwapBuffers(window);
+        }
+
         static Mesh LoadMeshResource(MeshResource &res) {
             //Generate buffers
             unsigned int vbo;
@@ -33,10 +99,12 @@ namespace Expl {
             glBindVertexArray(vao); //Bind vertex array buffer
 
             glBindBuffer(GL_ARRAY_BUFFER, vbo);
-            glBufferData(GL_ARRAY_BUFFER, static_cast<GLsizeiptr>(res.vertices.size() * sizeof(float)), res.vertices.data(), GL_STATIC_DRAW);
+            glBufferData(GL_ARRAY_BUFFER, static_cast<GLsizeiptr>(res.vertices.size() * sizeof(float)),
+                         res.vertices.data(), GL_STATIC_DRAW);
 
             glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-            glBufferData(GL_ELEMENT_ARRAY_BUFFER, static_cast<GLsizeiptr>(res.indices.size() * sizeof(int)), res.indices.data(), GL_STATIC_DRAW);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, static_cast<GLsizeiptr>(res.indices.size() * sizeof(int)),
+                         res.indices.data(), GL_STATIC_DRAW);
 
             //Set the attribute in the glsl
             //Setup layout(location = 0)
@@ -58,8 +126,8 @@ namespace Expl {
                     .VBO = vbo,
                     .VAO = vao,
                     .EBO = ebo,
-                    .VertexCount = (int)res.vertices.size(),
-                    .IndexCount = (int)res.indices.size(),
+                    .VertexCount = (int) res.vertices.size(),
+                    .IndexCount = (int) res.indices.size(),
                     .Mode = mode
             };
         }
@@ -73,6 +141,42 @@ namespace Expl {
             glBindVertexArray(0);
         }
     };
+
+    GLenum glCheckError_(const char *file, int line) {
+        GLenum errorCode;
+        while (errorCode = glGetError(), errorCode != GL_NO_ERROR) {
+            std::string error;
+            switch (errorCode) {
+                case GL_INVALID_ENUM:
+                    error = "INVALID_ENUM";
+                    break;
+                case GL_INVALID_VALUE:
+                    error = "INVALID_VALUE";
+                    break;
+                case GL_INVALID_OPERATION:
+                    error = "INVALID_OPERATION";
+                    break;
+                case GL_STACK_OVERFLOW:
+                    error = "STACK_OVERFLOW";
+                    break;
+                case GL_STACK_UNDERFLOW:
+                    error = "STACK_UNDERFLOW";
+                    break;
+                case GL_OUT_OF_MEMORY:
+                    error = "OUT_OF_MEMORY";
+                    break;
+                case GL_INVALID_FRAMEBUFFER_OPERATION:
+                    error = "INVALID_FRAMEBUFFER_OPERATION";
+                    break;
+                default:
+                    break;
+            }
+            std::cout << error << " | " << file << " (" << line << ")" << '\n';
+        }
+        return errorCode;
+    }
+
+#define glCheckError() glCheckError_(__FILE__, __LINE__)
 }
 
 #endif //EXPLORATION_RENDERER_H
